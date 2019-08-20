b0db529b6e0230ef69b6a94f2c90897b330dd650
Craig Fay <craigfay1@gmail.com>
2019-08-19 21:12:29 -0500

    Update Seec

--- data/seed.ts
+++ data/seed.ts
@@ -9,6 +9,15 @@ async function main() {
     email: 'maddy@prisma.io',
     name: 'Maddy',
   })
+  await prisma.createThread({
+    participants: {
+      connect: [{
+        email: "julien@prisma.io"
+      }, {
+        email: "maddy@prisma.io"
+      }]
+    }
+  })
 }
 
 main().catch(e => console.error(e))
