74a5e3bf38260b1123ac1ac92f54f80d8a5d3590
Craig Fay <craigfay1@gmail.com>
2019-08-24 00:03:43 -0500

    Add users to seed

--- data/users.seed.ts
+++ data/users.seed.ts
@@ -2,11 +2,23 @@ import { prisma } from '../generated/prisma-client'
 
 export default async function() {
   await prisma.createUser({
+    email: 'alan@prisma.io',
+    name: 'Alan',
+  })
+  await prisma.createUser({
     email: 'robert@prisma.io',
     name: 'Robert',
   })
   await prisma.createUser({
-    email: 'alan@prisma.io',
-    name: 'Alan',
+    email: 'joan@prisma.io',
+    name: 'Joan',
+  })
+  await prisma.createUser({
+    email: 'hugh@prisma.io',
+    name: 'Hugh',
+  })
+  await prisma.createUser({
+    email: 'alastair@prisma.io',
+    name: 'Alastair',
   })
 }
