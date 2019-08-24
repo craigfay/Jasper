dd6fa42a945a9b4c8bdce769f76ac208d38d5166
Craig Fay <craigfay1@gmail.com>
2019-08-24 00:04:48 -0500

    Add new thread seed

---
+++ data/thread_3.ts
@@ -0,0 +1,52 @@
+import { prisma } from '../generated/prisma-client'
+
+export default async function() {
+  const thread = await prisma.createThread({
+    participants: {
+      connect: [{
+        email: "alistair@prisma.io"
+      }, {
+        email: "alan@prisma.io"
+      }]
+    }
+  })
+  await prisma.createMessage({
+    content: "If you run the wires across the plugboard matrix diagonally, you'll eliminate rotor positions 500 times faster.",
+    thread: {
+      connect: {
+        id: thread.id
+      }
+    },
+    author: {
+      connect: {
+        email: "hugh@prisma.io"
+      }
+    }
+  })
+  await prisma.createMessage({
+    content: "This is actually not an entirely terrible idea.",
+    thread: {
+      connect: {
+        id: thread.id
+      }
+    },
+    author: {
+      connect: {
+        email: "alan@prisma.io"
+      }
+    }
+  })
+  await prisma.createMessage({
+    content: "That's Alan for \"thank you.\"",
+    thread: {
+      connect: {
+        id: thread.id
+      }
+    },
+    author: {
+      connect: {
+        email: "joan@prisma.io"
+      }
+    }
+  })
+}
